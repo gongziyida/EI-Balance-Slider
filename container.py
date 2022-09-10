9a7945561e926e62995ea4f479dcba71abf3397e
gongziyida <ziyi.gong@outlook.com>
2022-09-10 12:38:11 -0400

    working

---
+++ container.py
@@ -0,0 +1,43 @@
+import numpy as np
+
+class MeanInput:
+    NE, NI, NX = 20000, 5000, 5000
+    KE = NE * 0.04
+    KI = NI * 0.04
+    KX = NX * 0.04
+    JEE = 0.04
+    JEI = 0.11
+    JIE = 0.02
+    JII = 0.05
+    JEX = 0.06
+    JIX = 0.02
+    rX = 0.1
+    r_range = (0, 1)
+    def __init__(self, resolution=500):
+        self.res = resolution
+        self._r = np.linspace(*MeanInput.r_range, num=resolution)
+        self._rE, self._rI = np.meshgrid(self._r, self._r)
+    
+    def landscapes(self):
+        eqE = np.sqrt(self.KE) * self.JEE * self._rE - \
+              np.sqrt(self.KI) * np.abs(self.JEI) * self._rI + \
+              np.sqrt(self.KX) * self.JEX * self.rX
+        eqI = np.sqrt(self.KE) * self.JIE * self._rE - \
+              np.sqrt(self.KI) * np.abs(self.JII) * self._rI + \
+              np.sqrt(self.KX) * self.JIX * self.rX
+
+        eq = np.abs(eqE) + np.abs(eqI)
+        rImin_idx, rEmin_idx = np.unravel_index(eq.argmin(), eqE.shape)
+        rEmins, rImins = self._r[rEmin_idx], self._r[rImin_idx]
+        eqEmin, eqImin = eqE[rEmin_idx, rImin_idx], eqI[rEmin_idx, rImin_idx]
+        
+        return eq, eqE, eqI, eqEmin, eqImin, rEmins, rImins
+    
+    def ratios(self):
+        return self.JEE/self.JIE, self.JEI/self.JII, self.JEX/self.JIX, \
+               self.JIE/self.JII, np.sqrt(self.KI/self.KE)
+    
+    def reset(self):
+        for i in ('NE','NI','NX','KE','KI','KX',
+                  'JEE','JEI','JEX','JIE','JII','JIX','rX', 'r_range'):
+            setattr(self, i, getattr(MeanInput, i))
--- main.py
+++ main.py
@@ -1,83 +1,104 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, Button
+from matplotlib.colors import TwoSlopeNorm
+from container import MeanInput
 
+class SliderSpec(Slider):
+    def on_changed(self, func):
+        return super().on_changed(lambda v: func(self.label.get_text(), v))
 
-# The parametrized function to be plotted
-def f(KE, KI, KX, JEE, JEI, JEX, JIE, JII, JIX, rX, r_range=(0, 1), res=500):
-    _r = np.linspace(*r_range, num=res)
-    rE, rI = np.meshgrid(_r, _r)
+def im_range(eq):
+    vmin, vmax = eq.min(), eq.max()
+    if vmin > 0: # +- vmax covers all
+        return -vmax, vmax
+    elif vmax < 0: # +- vmin covers all
+        return vmin, -vmin
+    else: 
+        assert vmin < 0 and vmax > 0
+        v = max(vmax, -vmin)
+        return -v, v
     
-    gamma_I = np.sqrt(KI / KE)
-    gamma_X = np.sqrt(KX / KE)
-    eq1 = np.abs(JEE * rE - gamma_I * np.abs(JIE) * rI + gamma_X * JEX * rX)
-    eq2 = np.abs(JIE * rE - gamma_I * np.abs(JII) * rI + gamma_X * JIX * rX)
-    
-    rEmin_idx, rImin_idx = np.unravel_index((np.abs(eq1) + np.abs(eq2)).argmin(), eq1.shape)
-    rEmins, rImins = _r[rEmin_idx], _r[rImin_idx]
-    
-    return _r, eq1, eq2, rEmins, rImins
+def format_str(rEmins, rImins):
+    if hasattr(rEmins, '__iter__'):
+        rEstr = ', '.join(map(lambda v: '%.3f' % v, rEmins))
+        rIstr = ', '.join(map(lambda v: '%.3f' % v, rImins))
+    else:
+        rEstr = '%.3f' % rEmins
+        rIstr = '%.3f' % rImins
+    return rEstr, rIstr
 
-if __name__ == "__main__":
-    # Define initial parameters
-    NE, NI, NX = 20000, 5000, 5000
-    KE = NE * 0.04
-    KI = NI * 0.04
-    KX = NX * 0.04
-    JEE = 0.04
-    JEI = 0.11
-    JIE = 0.02
-    JII = 0.05
-    JEX = 0.06
-    JIX = 0.02
-    rX = 0.1
+if __name__ == '__main__':
+    p = MeanInput()
 
     # Create the figure and the line that we will manipulate
-    fig, ax = plt.subplots(1, 2, figsize=(10, 5), sharey='all', sharex='all')
-    r_vals, eqE, eqI, rEmins, rImins = f(KE, KI, KX, JEE, JEI, JEX, JIE, JII, JIX, rX)
-    extent = [r_vals[0], r_vals[-1], r_vals[0], r_vals[-1]]
-    imE = ax[0].imshow(eqE, origin='lower', extent=extent)
-    imI = ax[1].imshow(eqI, origin='lower', extent=extent)
+    fig, ax = plt.subplots(1, 3, figsize=(8, 5))
+    eq, eqE, eqI, eqEmin, eqImin, rEmins, rImins = p.landscapes()
+    eqE = TwoSlopeNorm(vcenter=0.)(eqE)
+    eqI = TwoSlopeNorm(vcenter=0.)(eqI)
+    extent = [*p.r_range, *p.r_range]
+    im0 = ax[2].imshow(eq, origin='lower', extent=extent, cmap='Greys')
+    imE = ax[0].imshow(eqE, origin='lower', extent=extent, cmap='Spectral')
+    imI = ax[1].imshow(eqI, origin='lower', extent=extent, cmap='Spectral')
+    marker0, = ax[2].plot(rEmins, rImins, marker='o', c='r', ms='8')
     markerE, = ax[0].plot(rEmins, rImins, marker='o', c='r', ms='8')
     markerI, = ax[1].plot(rEmins, rImins, marker='o', c='r', ms='8')
-    ax[0].set_xlabel('rE')
-    ax[0].set_ylabel('rI')
+    for i in range(3):
+        ax[i].set_xlabel('rE')
+        ax[i].set_ylabel('rI')
     ax[0].set_title('Mean Input to E')
     ax[1].set_title('Mean Input to I')
+    ax[2].set_title('Sum of Abs Mean Inputs')
 
-    fig.tight_layout()
 
     # adjust the main plot to make room for the sliders
-    fig.subplots_adjust(left=0.25, bottom=0.25)
+    fig.subplots_adjust(bottom=0.6)
 
+    txt = fig.text(0.2, 0.42, 'rE* = %s, rI* = %s\neqE* = %.3f, eqI* = %.3f' % \
+                   (*format_str(rEmins, rImins), eqEmin, eqImin))
+    txt = fig.text(0.5, 0.42, ('EE/IE = %.2f, EI/II = %.2f, EX/IX = %.2f\n'+ \
+                               'IE/II = %.2f, sqrt(K_I/K_E) = %.2f') % \
+                   p.ratios())
+    
     # The function to be called anytime a slider's value changes
-    def update(val):
-        r_vals, eqE, eqI, rEmins, rImins = f(KE, KI, KX, JEE, JEI, JEX, JIE, JII, JIX, rX)
+    def update(key, val):
+        setattr(p, key, val)
+        eq, eqE, eqI, eqEmin, eqImin, rEmins, rImins = p.landscapes()
+        eqE = TwoSlopeNorm(vcenter=0.)(eqE)
+        eqI = TwoSlopeNorm(vcenter=0.)(eqI)
+        txt.set_text('rE* = %s, rI* = %s\neqE* = %.3f, eqI* = %.3f' % \
+                     (*format_str(rEmins, rImins), eqEmin, eqImin))
+        extent = [*p.r_range, *p.r_range]
+        im0.set_data(eq)
         imE.set_data(eqE)
         imI.set_data(eqI)
+        marker0.set_data([rEmins, rImins])
         markerE.set_data([rEmins, rImins])
         markerI.set_data([rEmins, rImins])
         fig.canvas.draw_idle()
 
     # # Make horizontal sliders
     ax_sliders = {}
-    for i, (n, v) in enumerate(zip(('JEE', 'JEI', 'JEX', 'JIE', 'JII', 'JIX', 'rX'), 
-                                   (JEE, JEI, JEX, JIE, JII, JIX, rX))):
-        ax_sl = plt.axes([0.25, 0.1-i*0.05, 0.65, 0.03])
-        sl = Slider(
+    for i, n in enumerate(('JEE', 'JEI', 'JEX', 'JIE', 'JII', 'JIX', 'rX')):
+        ax_sl = fig.add_axes([0.2, i*0.05+0.07, 0.65, 0.03])
+        sl = SliderSpec(
             ax=ax_sl,
             label=n,
             valmin=0.0,
-            valmax=1,
-            valinit=v,
+            valmax=0.5,
+            valinit=getattr(MeanInput, n),
         )
         sl.on_changed(update) # register the update function with each slider
+        ax_sliders[n] = (ax_sl, sl)
 
     # Create a `matplotlib.widgets.Button` to reset the sliders to initial values.
-    resetax = plt.axes([0.8, 0.15, 0.1, 0.04])
+    resetax = fig.add_axes([0.8, 0.02, 0.1, 0.04])
     def reset(event):
-        freq_slider.reset()
-        amp_slider.reset()
+        txt.set_text('rE* = %s, rI* = %s\neqE* = %.3f, eqI* = %.3f' % \
+                     (*format_str(rEmins, rImins), eqEmin, eqImin))
+        p.reset()
+        for k, (a, s) in ax_sliders.items():
+            s.reset()
     button = Button(resetax, 'Reset', hovercolor='0.975')
     button.on_clicked(reset)
     
